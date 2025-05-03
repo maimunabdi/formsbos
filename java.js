839efe53a1c940c2555deb68171e76a70d9e1b62
maimunabdi <maymuuncabdi725@gmail.com>
2025-05-03 01:57:59 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+
+
+
+
+//     function submitFeedback(event){
+//         event.preventDefault();
+
+
+//         const name =document.getElementById('name').value;
+//         const email =document.getElementById('email').value;
+//         const meassage =document.getElementById('message').value;
+
+
+//         if(name===""||email===""|| message===""){
+//             document.getElementById('responseMessage').innerText="all fied are required";
+//             return;
+//         }
+
+//         document.getElementById('responseMessage').innerText="Feedback submitted successfully!";
+
+//     }
+// document.getElementById('feedbackForm').reset();
+
+
+    
